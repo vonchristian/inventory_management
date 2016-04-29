d0d24d4bc5449fe23d2417eeed07b19fbe6e5635
Von Christian Halip <vc.halip@gmail.com>
2016-04-29 21:03:44 +0800

    Display store.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "products#index"
-
+  root to: 'store#index', as: 'store'
+  get "store/index"
   resources :products
 end
