51becf69410d8f9acabd57f66570a269f281d195
Von Christian Halip <vc.halip@gmail.com>
2016-08-15 02:08:37 +0800

    Dont daemonize

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 
 environment ENV['RAILS_ENV'] || 'production'
 
-daemonize true
+# daemonize true
 
 pidfile "/var/www/inventory_management/shared/tmp/pids/puma.pid"
 stdout_redirect "/var/www/inventory_management/shared/tmp/log/stdout", "/var/www/inventory_management/shared/tmp/log/stderr"
