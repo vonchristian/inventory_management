bc972c33e1a46dcd79fb1c11b7dd73a33659ceef
Christopher Pumihic <nnittop_909@live.com>
2016-08-16 23:11:13 +0800

    chmod.

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
-# #!/usr/bin/env puma
+#!/usr/bin/env puma
 
-# environment ENV['RAILS_ENV'] || 'production'
+environment ENV['RAILS_ENV'] || 'production'
 
-# daemonize true
+daemonize true
 
-# pidfile "/var/www/inventory_management/shared/tmp/pids/puma.pid"
-# stdout_redirect "/var/www/inventory_management/shared/tmp/log/stdout", "/var/www/inventory_management/shared/tmp/log/stderr"
+pidfile "/var/www/inventory_management/shared/tmp/pids/puma.pid"
+stdout_redirect "/var/www/inventory_management/shared/tmp/log/stdout", "/var/www/inventory_management/shared/tmp/log/stderr"
 
-# threads 0, 16
+threads 0, 16
 
-# bind "unix:///var/www/inventory_management/shared/tmp/sockets/puma.sock"
+bind "unix:///var/www/inventory_management/shared/tmp/sockets/puma.sock"
